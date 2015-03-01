adf02b944ca07bb834e3bdd2a2f5e948682fe6a4
dredupuika <andrievs.niedra@inbox.lv>
2015-03-01 15:14:29 +0200

    handle nfc inputs

---
+++ www/js/script2.js
@@ -0,0 +1,20 @@
+
+jQuery(function()
+{
+
+  var container = jQuery('body');
+
+  container.on('focus', 'input.nfc', function()
+  {
+    var target = jQuery(this);
+    Nfc.on('tag_read', function()
+    {
+      target.val( Nfc.tag );
+      Nfc.unbind('tag_read');
+    });
+  });
+
+
+
+
+});
