f15e2c9c943941b687396a52a5120d21e4e4431a
Matīss Strautmanis <matiss.strautmanis@gmail.com>
2015-03-28 20:21:11 +0200

    base64

--- www/js/pictures.js
+++ www/js/pictures.js
@@ -1,14 +1,14 @@
 function takePicture() {
-    navigator.camera.getPicture(function(uri) {
+    navigator.camera.getPicture(function onSuccess(imageData) {
     var img = document.getElementById('camera_image');
     img.style.visibility = "visible";
     img.style.display = "block";
-    img.src = uri;
+    img.src = "data:image/jpeg;base64," + imageData;
     document.getElementById('camera_status').innerHTML = "Success";
     },
     function(e) {
        console.log("Error getting picture: " + e);
         document.getElementById('camera_status').innerHTML = "Error getting picture.";
         },
-        { quality: 50, destinationType: navigator.camera.DestinationType.FILE_URI});
+        { quality: 50, destinationType: navigator.camera.DestinationType.DATA_URL});
       };
